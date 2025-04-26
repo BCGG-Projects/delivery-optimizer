befcb7ae690a475879eb660f1e6fc4c44db85382
adamo <adamowiczkanapkowy@gmail.com>
2025-04-26 14:14:38 +0200

    Completed points regarding:
    # define with points want the goods to be delivered to them and some points want boolean 1 | 0, and total weight
    
    #created the size of the goods to be be delivered or collected (between 100kg and 200kg);
    
    #random the amount of goods, their type and delivery or collect status

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from data_generator import DataGenerator
+from warehouses_logic import WarehousesLogic
 from utils import to_json
 
 if __name__ == "__main__":
     generator = DataGenerator()
     input_data = generator.generate()
     print(to_json(input_data))
+
+    print(WarehousesLogic(input_data["warehouses"]).show_warehouses())
