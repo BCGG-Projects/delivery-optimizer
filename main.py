bae98ee121fd322ff2f5645d7a056246021d67d6
adam.grzana <adam.grzana@peritus-act.com>
2025-05-07 14:28:02 +0200

    Remove unnecessary WarehousesLogic class
    Refactor generate_clients to change the logic for total_weight, delivery_type, and goods assignment.
    
    Move distribute_weight method into DataGenerator class for better encapsulation.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from data_generator import DataGenerator
-from warehouses_logic import WarehousesLogic
 from utils import to_json
 
 if __name__ == "__main__":
@@ -7,4 +6,4 @@ if __name__ == "__main__":
     input_data = generator.generate()
     print(to_json(input_data))
 
-    print(WarehousesLogic(input_data["warehouses"]).show_warehouses())
+    
